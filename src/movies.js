91c409d4614dd434b1ad7dddafe1a96ab1fc49f3
Maria Suarez <msuarezh@gmail.com>
2023-02-19 14:33:25 +0100

    iteration 6

--- src/movies.js
+++ src/movies.js
@@ -36,10 +36,18 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const moviesByYear = [...moviesArray].sort((a,b) => a.title - b.title)
+    moviesByYear.sort((a,b) => a.year - b.year)
+    return moviesByYear;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    let moviesByAlphabet = [...moviesArray].map((movie) => movie = movie.title);
+    moviesByAlphabet = moviesByAlphabet.sort();
+    return moviesByAlphabet.slice(0, 20);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
